d2d9b77984c6036264b8f95066e7cd176d6caa63
ILshat Khamitov <admin@site15.ru>
2019-11-06 01:47:20 +0500

    fix: Remove imports HttpClientModule in NgxColdModule

--- libs/ngx-cold/src/lib/ngx-cold.module.ts
+++ libs/ngx-cold/src/lib/ngx-cold.module.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { NgxColdClickDirective } from './ngx-cold-click.directive';
 import { NgxColdFormDirective } from './ngx-cold-form.directive';
 
 export const NGX_COLD_DIRECTIVES = [NgxColdClickDirective, NgxColdFormDirective];
 @NgModule({
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule],
   declarations: NGX_COLD_DIRECTIVES,
   exports: NGX_COLD_DIRECTIVES
 })
